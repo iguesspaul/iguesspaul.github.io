eb64193d18ae07234463ef360d500782dc23cb08
iguesspaul <paul.l.vantilburg@gmail.com>
2022-12-08 11:13:01 -0500

    sdfsd

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,6 @@ import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter()
