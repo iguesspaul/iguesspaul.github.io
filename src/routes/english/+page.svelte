2b8a5fbd4b6150b63bc811ec35f952223db3296f
iguesspaul <paul.l.vantilburg@gmail.com>
2022-12-09 08:29:18 -0500

    fixed titles

--- src/routes/english/+page.svelte
+++ src/routes/english/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex flex-col items-start p-3">
-    <h1 class="mb-2 text-2xl font-bold">Grade 8</h1>
+    <h1 class="mb-2 text-2xl font-bold">Grade 9</h1>
     <a id="tkam" class="h-auto p-2 font-medium bg-teal-100 rounded-lg" href="https://www.litcharts.com/lit/to-kill-a-mockingbird">TKAM guide</a>
 </div>
--- src/routes/math/+page.svelte
+++ src/routes/math/+page.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <div class="flex flex-col items-start p-3">
-	<h1 class="mb-2 text-2xl font-bold">Grade 8</h1>
+	<h1 class="mb-2 text-2xl font-bold">Grade 9</h1>
 	<a id="r17">
 		<button on:click={() => (r17 = !r17)} class="h-auto p-2 font-medium bg-teal-100 rounded-lg"
 			>R.17 Calculator</button
