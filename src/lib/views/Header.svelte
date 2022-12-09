e6ea5a53bc2597f7cbbd78b5fef011b5f2d610d4
iguesspaul <paul.l.vantilburg@gmail.com>
2022-12-09 11:26:57 -0500

    added link

--- src/lib/views/Header.svelte
+++ src/lib/views/Header.svelte
@@ -2,4 +2,5 @@
         <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/">Home</a>
         <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/english">English</a>
         <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/math">Math</a>
+        <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/science">Science</a>
 </header>
