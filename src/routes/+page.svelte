4b27f86484a81acf48b8138602b65ed50a7ee14d
iguesspaul <paul.l.vantilburg@gmail.com>
2022-12-13 14:59:31 +0100

    added chatgpt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <div class="flex flex-col items-start p-3">
-    <h1 class="mb-3 text-2xl font-bold">Popular</h1>
-    <a class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg" href="/math/#r17">R.17 Calculator</a>
-    <a class="h-auto p-2 font-medium bg-teal-100 rounded-lg" href="/english/#tkam">TKAM guide</a>
+	<h1 class="mb-3 text-2xl font-bold">Popular</h1>
+	<a class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg" href="/math/#r17">R.17 Calculator</a
+	>
+	<a class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg" href="/english/#tkam">TKAM guide</a>
+	<a class="h-auto p-2 font-medium bg-teal-100 rounded-lg" href="/english/#chatgpt"
+		>Ai Essay Writer</a
+	>
 </div>
-
--- src/routes/english/+page.svelte
+++ src/routes/english/+page.svelte
@@ -1,4 +1,14 @@
 <div class="flex flex-col items-start p-3">
-    <h1 class="mb-2 text-2xl font-bold">Grade 9</h1>
-    <a id="tkam" class="h-auto p-2 font-medium bg-teal-100 rounded-lg" href="https://www.litcharts.com/lit/to-kill-a-mockingbird">TKAM guide</a>
+	<h1 class="mb-2 text-2xl font-bold">Grade 9</h1>
+	<a
+		id="tkam"
+		class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg"
+		href="https://www.litcharts.com/lit/to-kill-a-mockingbird">TKAM guide</a
+	>
+	<a
+		id="chatgpt"
+		class="h-auto p-2 font-medium bg-teal-100 rounded-lg"
+		href="https://www.litcharts.com/lit/to-kill-a-mockingbird"
+		>Chat GPT (writes essays and essays titles)</a
+	>
 </div>
