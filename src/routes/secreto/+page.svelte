a6a89781c07ab8dc8f2f2c786883520e9970d481
iguesspaul <paul.l.vantilburg@gmail.com>
2023-01-03 15:07:56 -0500

    further development of homepage

--- src/routes/secreto/+page.svelte
+++ src/routes/secreto/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { goto } from '$app/navigation';
 	export let data: PageData;
 	const date: Date = new Date();
 	const day = date.getDate();
 	const month = date.getMonth();
+	const year = date.getFullYear();
 	const months = [
 		'January',
 		'February',
@@ -15,16 +17,16 @@
 		'August',
 		'September',
 		'October',
-		 'November',
-		 'December'
-    ];
-    const quotes = [
-        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-        "The way to get started is to quit talking and begin doing.",
-        "The only true wisdom is in knowing you know nothing.",
-        "Quality is not an act, it is a habit.",
-        "Know how to listen and you will profit even from those who talk badly."
-    ]
+		'November',
+		'December'
+	];
+	const quotes = [
+		'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+		'The way to get started is to quit talking and begin doing.',
+		'The only true wisdom is in knowing you know nothing.',
+		'Quality is not an act, it is a habit.',
+		'Know how to listen and you will profit even from those who talk badly.'
+	];
 	const append = (n: number) => {
 		const lastn = String(n);
 		const lastOf = Number(lastn[lastn.length - 1]);
@@ -34,9 +36,15 @@
 		return 'th';
 	};
 
-	const final = `Today is the ${day}${append(day)} of ${months[month]}.`;
-    const quote = quotes[Math.floor(Math.random() * (5))];
+	function googleSearch(e: KeyboardEvent) {
+		if (e.key === 'Enter') {
+			goto(`https://www.google.com/search?client=firefox-b-d&q=${search}`);
+		}
+	}
 
+	const final = `Today is the ${day}${append(day)} of ${months[month]}, ${year}.`;
+	const quote = quotes[Math.floor(Math.random() * 5)];
+	let search = '';
 </script>
 
 <main class="relative w-screen h-screen p-0 m-0">
@@ -79,29 +87,44 @@
 						<iconify-icon icon="cib:gmail" width="84" height="84" />
 					</a>
 				</div>
-				<div class="flex flex-col items-center justify-center col-span-1 row-span-2 p-4 bg-teal-50 rounded-2xl">
-                    <p class="text-3xl italic font-bold">"{quote}"</p>
-                </div>
-                <div class="flex items-center justify-center col-span-3 row-span-2 bg-teal-50 rounded-2xl">
-                    <img src="/giphy.gif" alt="animated gif" class="w-full h-full rounded-2xl"/>
-                </div>
+				<div
+					class="flex flex-col items-center justify-center col-span-1 row-span-2 p-4 bg-teal-50 rounded-2xl"
+				>
+					<p class="text-3xl italic font-bold">"{quote}"</p>
+				</div>
+				<div class="flex items-center justify-center col-span-3 row-span-2 bg-teal-50 rounded-2xl">
+					<img src="/giphy.gif" alt="animated gif" class="w-full h-full rounded-2xl" />
+				</div>
 			</div>
 		</div>
 		<div class="w-1/2 h-full bg-teal-50">
 			<div class="grid w-full h-full grid-cols-4 grid-rows-4 gap-4 p-4 bg-transparent">
-				<div class="flex-col col-span-2 row-span-2 p-3 text-center bg-teal-900 rounded-2xl">
-					<h1 class="mb-4 text-4xl font-bold text-white">Tasks</h1>
+				<div class="flex-col col-span-2 row-span-2 p-4 text-center bg-teal-900 rounded-2xl">
+					<h1 class="my-4 text-4xl font-bold text-white">Tasks</h1>
 					{#each data.res as task}
 						<h1 class="text-4xl font-medium text-white">- {task.content}</h1>
 					{:else}
 						<h1>tasks done</h1>
 					{/each}
 				</div>
-                <div class="flex items-center justify-center col-span-1 row-span-1 row-start-3 transition bg-teal-900 rounded-2xl active:scale-90 hover:cursor-pointer">
-                    <a href="https://todoist.com/app/today">
-                        <iconify-icon icon="cib:todoist" width="84" height="84" class="text-white"></iconify-icon>
-                    </a>
-                </div>
+				<div
+					class="flex items-center justify-center col-span-1 row-span-1 row-start-3 transition bg-teal-900 rounded-2xl active:scale-90 hover:cursor-pointer"
+				>
+					<a href="https://todoist.com/app/today">
+						<iconify-icon icon="cib:todoist" width="84" height="84" class="text-white" />
+					</a>
+				</div>
+				<div
+					class="flex items-center justify-center col-span-3 row-span-1 row-start-4 bg-teal-900 rounded-2xl"
+				>
+					<input
+						bind:value={search}
+						class="w-3/4 p-4 text-4xl font-bold text-black bg-teal-50 rounded-2xl"
+						placeholder="Google Search"
+						on:keydown={googleSearch}
+					/>
+					<iconify-icon class="ml-4 text-white " icon="cil:search" width="64" height="64" />
+				</div>
 			</div>
 		</div>
 	</div>
