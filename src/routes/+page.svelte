14266de235c0203ff3d51dfe6a2c96113314bd66
iguesspaul <paul.l.vantilburg@gmail.com>
2023-01-09 15:21:56 +0100

    homepage changes + layout fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,9 @@
 <Header />
 <div class="flex flex-col items-start p-3">
 	<h1 class="mb-3 text-2xl font-bold">Popular</h1>
-	<a class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg" href="/math/#r17">R.17 Calculator</a
+	<a class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg" href="/math/#r17">Distance Calculator</a
 	>
-	<a class="h-auto p-2 mb-3 font-medium bg-teal-100 rounded-lg" href="/english/#tkam">TKAM guide</a>
 	<a class="h-auto p-2 font-medium bg-teal-100 rounded-lg" href="/english/#chatgpt"
-		>Ai Essay Writer</a
+		>Romeo and Juliet Litnotes</a
 	>
 </div>
